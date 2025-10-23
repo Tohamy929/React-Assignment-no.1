0fabbc98bf276f3f1524950f7b95608678fecd6c
Tohamy929 <alytohamy3@gmail.com>
2025-10-23 15:57:39 +0200

    removed strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
@@ -7,7 +7,7 @@ import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
     <App />
-  </StrictMode>,
+  </>,
 )
